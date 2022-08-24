bde16b3d8e75addfb1577eb16f9c89f05fedb0de
Vishwanathhiremath112 <105048800+Vishwanathhiremath112@users.noreply.github.com>
2022-08-24 14:51:30 +0530

    Add files via upload

---
+++ ConsoleAppMath/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleaAppDemo
+{
+    class program1
+    {
+
+
+
+        public static void divide(int a, int b, out int rem, out int res)
+        {
+            rem = a % b;
+            res = a / b;
+
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("enter a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            divide(a, b, out int rem, out int res);
+            Console.WriteLine($"quotient is {res}, remainder is {rem}");
+        }
+    }
+}
